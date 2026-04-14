dd8fe121fbae8e94a86c8c0397e25b5fae1fe178
Koushik Karmakar <koushik9339mail@gmail.com>
2026-04-14 17:35:59 +0530

    simplify database connection configuration by using DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,21 @@
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  max: 20,
-  connectionTimeoutMillis: 0,
-  idleTimeoutMillis: 0,
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // max: 20,
+  // connectionTimeoutMillis: 0,
+  // idleTimeoutMillis: 0,
 });
 
 export default pool;
